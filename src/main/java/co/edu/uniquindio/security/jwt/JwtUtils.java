470aee065cf077b679a9256a4685a352ed2485c0
David <dlromerom@uqvirtual.edu.co>
2024-03-12 00:17:50 -0500

    se crean metodos de configuracion del token

--- src/main/java/co/edu/uniquindio/security/jwt/JwtUtils.java
+++ src/main/java/co/edu/uniquindio/security/jwt/JwtUtils.java
@@ -1,9 +1,18 @@
 package co.edu.uniquindio.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Date;
+
 @Component
+@Slf4j
 public class JwtUtils {
 
     @Value("${jwt.secret.key}")
@@ -12,6 +21,35 @@ public class JwtUtils {
     @Value("${jwt.time.expiration}")
     private String timeExpiration;
 
+    //Generar token de acceso
+    public String generateAccessToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(timeExpiration)))
+                .signWith(getSignatureKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
+    //validar el token de acceso
+    public boolean isTokenValid(String token){
+        try {
+            Jwts.parser().setSigningKey(getSignatureKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return true
+        }
+        catch (Exception e){
+            log.error("Token invalido, error: ".concat(e.getMessage()));
+            return false;
+        }
+    }
+
+    //Obtener firma del token
+    public Key getSignatureKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
 }
